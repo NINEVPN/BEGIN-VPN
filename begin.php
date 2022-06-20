3d58dfad93bbcac2e9fae22c7f3e95390f613f12
NINEVPN <103882719+NINEVPN@users.noreply.github.com>
2022-06-20 18:06:01 +0700

    Add files via upload

---
+++ begin.php
@@ -0,0 +1,8 @@
+{
+  "Version_update": "1.0",
+  "Link_zip": "https://github.com/NINEVPN/begin/blob/main/beginvpn.zip?raw=true",
+  "Password": "140690",
+  "Message": "BEGIN VPN
+กดอัปเดตเพื่อใช้งานVPN
+"
+}
