80c648f95d31e0dbcc859be418bb96a5bebd84d6
NINEVPN <103882719+NINEVPN@users.noreply.github.com>
2023-03-10 06:28:00 +0700

    Update begin.php

--- begin.php
+++ begin.php
@@ -1,8 +1,9 @@
 {
-  "Version_update": "2.2",
+  "Version_update": "2.3",
   "Link_zip": "https://github.com/NINEVPN/begin/blob/main/beginvpn.zip?raw=true",
   "Password": "140690",
   "Message": "BEGIN VPN
-Test 
+Test❗❗
+10/03/23
 "
 }
